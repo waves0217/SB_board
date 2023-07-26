0f8a4ae95dd093a7965aba2da26cf3cba643ab4a
waves0217 <waves0217@gmail.com>
2023-07-27 00:51:07 +0900

    설정변경, 컨트롤러 테스트

---
+++ SB_Board/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,24 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String root() {
+		return "root() 함수 호출테스트";
+	}
+	
+	
+	@RequestMapping("/listForm")
+	public String listForm() {
+		return "listForm";
+	}
+	
+	
+	
+}
--- SB_Board/src/main/java/com/study/springboot/git_test.java
+++ SB_Board/src/main/java/com/study/springboot/git_test.java
@@ -2,8 +2,8 @@ package com.study.springboot;
 
 public class git_test {
 
-	public static void main(String[] args) {
-		System.out.println("git 테스트");
-	}
+//	public static void main(String[] args) {
+//		System.out.println("git 테스트");
+//	}
 
 }
