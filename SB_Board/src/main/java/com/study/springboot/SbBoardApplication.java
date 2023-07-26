db5fd5a0f2218678a2e48183cbb58bf679b60cb0
waves0217 <waves0217@gmail.com>
2023-07-26 23:12:27 +0900

    git 테스트

---
+++ SB_Board/src/main/java/com/study/springboot/SbBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBoardApplication.class, args);
+	}
+
+}
---
+++ SB_Board/src/main/java/com/study/springboot/git_test.java
@@ -0,0 +1,10 @@
+package com.study.springboot;
+
+public class git_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git 테스트");
+	}
+
+}
